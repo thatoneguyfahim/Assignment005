abafd9c1fcd813fec25458a124b9e1e822264286
thatoneguyfahim <1605059@ugrad.cse.buet.ac.bd>
2024-09-24 21:31:22 +0600

    second commit

--- js/donate.js
+++ js/donate.js
@@ -9,7 +9,39 @@ document.getElementById('donate').addEventListener('click', function(){
     document.getElementById('donation').innerText = newDonation;
     }
     else {
-        alert("Not a number")
+        alert("Inappropriate Donation Amount")
+    }
+
+})
+
+document.getElementById('donate-feni').addEventListener('click', function(){
+    const base = getNumber('base-amount')
+    const donateAmount = parseFloat(document.getElementById('donate-amount-feni').value)
+    if (donateAmount >= 0){
+    const donation = getNumber('donation-amount-feni')
+    const newBase = base - donateAmount;
+    const newDonation = donation+donateAmount;
+    document.getElementById('base-amount').innerText = newBase;
+    document.getElementById('donation-amount-feni').innerText = newDonation;
+    }
+    else {
+        alert("Inappropriate Donation Amount")
+    }
+
+})
+
+document.getElementById('donate-quota').addEventListener('click', function(){
+    const base = getNumber('base-amount')
+    const donateAmount = parseFloat(document.getElementById('donate-amount-quota').value)
+    if (donateAmount >= 0){
+    const donation = getNumber('donation-amount-quota')
+    const newBase = base - donateAmount;
+    const newDonation = donation+donateAmount;
+    document.getElementById('base-amount').innerText = newBase;
+    document.getElementById('donation-amount-quota').innerText = newDonation;
+    }
+    else {
+        alert("Inappropriate Donation Amount")
     }
 
 })
